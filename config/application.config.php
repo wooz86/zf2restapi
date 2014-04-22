c4a897910f4690b761b63e1a5d5d5861a53cedf0
Joel Thoresson <joelt86@hotmail.com>
2014-04-22 22:38:32 +0200

    Enabled missing dependency modules

--- config/application.config.php
+++ config/application.config.php
@@ -2,6 +2,11 @@
 return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
+        'DoctrineModule',
+        'DoctrineORMModule',
+        'DoctrineMigrationsModule',
+        'ZF\ApiProblem',
+        'ZF\ContentNegotiation',
         'Core',
         'Application',
     ),
